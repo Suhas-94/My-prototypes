09eb78f15fc12c356a64a0e14ec628dd8fad8ee8
Suhas-94 <142117542+Suhas-94@users.noreply.github.com>
2023-10-26 21:38:09 +0530

    Add files via upload

---
+++ Myprototype/Login/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".btn-large").click(function(){    
+    
+    var username=$(".username").val();
+    var password=$(".password").val();
+   
+    if(username === "Admin" && password === "12345"){
+    
+        
+       
+     
+      
+    }else{
+    
+        alert("Invalid username or password");
+    }
+    
+    
+    });
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
